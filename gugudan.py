752bee8ed1d83167ae9ce99dce3ccddd093d4749
JaeHyun Hwang <hjh6195@naver.com>
2020-04-17 11:29:23 +0900

    Made GuGu-Dan, but something is wrong

---
+++ gugudan.py
@@ -0,0 +1,24 @@
+def Dan(num):
+    print(str(num)+" Dan:")
+    print(str(num)+" * "+str(1)+" = "+str(num*1))
+    print(str(num)+" * "+str(2)+" = "+str(num*2))
+    print(str(num)+" * "+str(3)+" = "+str(num*3))
+    print(str(num)+" * "+str(4)+" = "+str(num*4))
+    print(str(num)+" * "+str(5)+" = "+str(num*5))
+    print(str(num)+" * "+str(6)+" = "+str(num*6))
+    print(str(num)+" * "+str(7)+" = "+str(num*7))
+    print(str(num)+" * "+str(8)+" = "+str(num*8))
+    print(str(num)+" * "+str(9)+" = "+str(num*9))
+    print()
+    
+def GuGu():
+    Dan(1)
+    Dan(2)
+    Dan(4) # I think something is missing
+    Dan(5)
+    Dan(6)
+    Dan(7)
+    Dan(8)
+    Dan(9)
+
+GuGu()
